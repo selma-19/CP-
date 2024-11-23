7eed46ca9b3500beb6a260e0cc443b41ee7afa37
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-11-23 17:21:11 +0100

    delete from a trie

--- Trie data structure (prefix tree)/main.cpp
+++ Trie data structure (prefix tree)/main.cpp
@@ -12,7 +12,7 @@ struct TrieNode{
 };
 class Trie {
 public:
-    TrieNode* root;
+    TrieNode* root= new TrieNode();
 
     void insert(string s) {
         TrieNode* currentNode = root;
@@ -46,4 +46,66 @@ public:
         }
         return true;
     }
+    bool isEmpty(TrieNode* root){
+        if(!root)
+            return false;
+            for(int i = 0;i<26;i++){
+                if(root->children[i])
+                    return false;
+            }
+
+        return true ;
+    };
+
+    TrieNode* remove(string key){
+       return remove(root,key);
+    }
+    TrieNode* remove(TrieNode* root , string key ,int depth = 0){
+        if(!root)
+            return root;
+        else if(depth == key.size()) {
+            if(isEmpty(root)){
+                delete(root);
+                return nullptr;
+        }
+            else {
+                root->isEnding = false;
+                return root;
+            }
+    }
+        if(root->children[key[depth]-'a']){
+            root->children[key[depth]-'a']= remove(root->children[key[depth]-'a'],key,depth+1);
+        }
+
+        // a root does not have any child and is not ending of the word
+        if (isEmpty(root) && root->isEnding == false) {
+            root = nullptr;
+            delete (root);
+        }
+        return root;
 };
+};
+int main()
+{
+    // Input keys (use only 'a' through 'z'
+    // and lower case)
+    string keys[] = { "the", "a", "there",
+                      "answer", "any", "by",
+                      "bye", "their", "hero", "heroplane" };
+    int n = sizeof(keys) / sizeof(keys[0]);
+
+    Trie* root = new Trie();
+
+    // Construct trie
+    for (int i = 0; i < n; i++)
+        root->insert( keys[i]);
+
+    // Search for different keys
+    root->search("the") ? cout << "Yes\n" : cout << "No\n";
+    root->search("these") ? cout << "Yes\n" : cout << "No\n";
+
+    root->remove( "heroplane");
+    root->startsWith( "heropla") ? cout << "Yes\n" : cout << "No\n";
+    root->search( "hero") ? cout << "Yes\n" : cout << "No\n";
+    return 0;
+}
