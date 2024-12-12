5b25fd89598a7606f7931c54a07c4d104db56ec9
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-12-13 00:20:41 +0100

    maximum xormaximum xor

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int value;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int v){
+        left=nullptr;
+        right=nullptr;
+        value=v;
+    }
+};
